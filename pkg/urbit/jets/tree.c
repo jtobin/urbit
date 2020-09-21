4ef6e9607c06143e3b387c82054c630f22f7f6b9
Jared Tobin <jared@jtobin.io>
2020-09-21 13:03:59 -0230

    jets: remove ut_crop, ut_fuse, ut_mint

--- pkg/urbit/jets/tree.c
+++ pkg/urbit/jets/tree.c
@@ -472,21 +472,6 @@ static c3_c* _141_pen_loot_ha[] = {
   0
 };
 
-  static u3j_harm _141_pen__ut_crop_a[] = {{".2", u3wfu_crop}, {}};
-  static c3_c* _141_pen__ut_crop_ha[] = {
-    "d83e5e47f712870aba815d79943d287cbefdc00640409464b30bf755115d4a1a",
-    0
-  };
-  static u3j_harm _141_pen__ut_fuse_a[] = {{".2", u3wfu_fuse}, {}};
-  static c3_c* _141_pen__ut_fuse_ha[] = {
-    "43d8bfdf9255f548bb58d9975bac273e2dcebe5ae98bd7e466b6fff6ff43a944",
-    0
-  };
-  static u3j_harm _141_pen__ut_mint_a[] = {{".2", u3wfu_mint}, {}};
-  static c3_c* _141_pen__ut_mint_ha[] = {
-    "43a06316365bcd14a94f8ed1f3fe5a8f61d1da5bea989296a192b62a966fca11",
-    0
-  };
   static u3j_harm _141_pen__ut_mull_a[] = {{".2", u3wfu_mull}, {}};
   static c3_c* _141_pen__ut_mull_ha[] = {
     "9fe555b3f9ad666f04194037437d71ee98f6b884f7aacc46a11ad27407cb7e8e",
@@ -514,9 +499,6 @@ static c3_c* _141_pen_loot_ha[] = {
 
 static u3j_core _141_pen__ut_d[] =
   {
-    { "crop", 7, _141_pen__ut_crop_a, 0, _141_pen__ut_crop_ha },
-    { "fuse", 7, _141_pen__ut_fuse_a, 0, _141_pen__ut_fuse_ha },
-    { "mint", 7, _141_pen__ut_mint_a, 0, _141_pen__ut_mint_ha },
     { "mull", 7, _141_pen__ut_mull_a, 0, _141_pen__ut_mull_ha },
     { "nest", 7, 0, _141_pen__ut_nest_d, _141_pen__ut_nest_ha },
     { "rest", 7, _141_pen__ut_rest_a, 0, _141_pen__ut_rest_ha },
@@ -550,6 +532,7 @@ static u3j_hood _141_pen__ut_ho[] = {
   { "felt",      94 },
   { "fine",   49086 },
   { "fire",       4 },
+  { "fish",    6006 },
   { "fond",   12283 },
   { "fund",    6014 },
   //  XX +funk is not part of +ut, and this hook appears to be unused
